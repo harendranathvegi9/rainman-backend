851aad83d31e202dc1f08361859f47a23dfa5547
Seth Thompson <s3th.thompson@gmail.com>
2013-11-09 11:02:05 -0500

    added cross origin

--- server.py
+++ server.py
@@ -1,8 +1,52 @@
 import os
-from flask import Flask, request,jsonify
+from flask import Flask, request,jsonify, make_response, current_app
 import nltk
 from readability.readability import Document
 import wikipedia
+from datetime import timedelta
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 MIN_LEN = 0
 
@@ -32,14 +76,47 @@ def handle_error(error):
 class Filters:
 
 	def __init__(self):
-		self.functions = [self.tokenize_words, self.proper_nouns]
+		self._ne = []
+		self.functions = [self.named_entities]
 		return
+
+	def named_entities(self, content, domain):
+		sentences = nltk.sent_tokenize(content['raw'])
+		sentences = [nltk.word_tokenize(sent) for sent in sentences]
+		sentences = [nltk.pos_tag(sent) for sent in sentences]
+		trees = nltk.batch_ne_chunk(sentences)
+		for tree in trees:
+			self._traverse(tree)
+		return self._ne, []
+
+
+	def _traverse(self,t):
+		ne = [
+			'ORGANIZATION',
+			'PERSON',
+			'LOCATION',
+			'DATE',
+			'TIME',
+			'MONEY',
+			'PERCENT',
+			'FACILITY',
+			'GPE'
+		]
+		try:
+			t.node
+		except AttributeError:
+			return
+		else:
+			if t.node in ne:
+				if t not in self._ne:
+					self._ne.append(t)
+			else:
+				for child in t:
+					self._traverse(child)
 
 	def tokenize_words(self, content, domain):
 		content['tokens'] = nltk.word_tokenize(content['raw'].encode('utf-8'))
 		return content, []
-
- 
 
 	def _wikipedia_card(self,query):
 		try:
@@ -91,8 +168,7 @@ class Filters:
 def rainman(full_html, domain):
 	content = parse(full_html, domain)
 	cards = []
-	#content, cards = run_filters(content, domain)
-
+	content, cards = run_filters(content, domain)
 	return jsonify(content=content['readable'], cards=cards)
 
 def parse(full_html, domain):
@@ -133,6 +209,7 @@ def home():
 	return 'Hello World!'
 
 @app.route('/api', methods=['POST'])
+@crossdomain(origin='*')
 def api():
 	content = request.form['content']
 	domain = request.form['domain']
