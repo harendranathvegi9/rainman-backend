a8ee639b18d884c3d8b23071688d67fd7d8d7ee3
Seth Thompson <s3th.thompson@gmail.com>
2013-11-08 19:38:31 -0500

    debug requests

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return 'Hello World!'
+@app.route('/api', methods=['POST'])
+def parse():
+	#assert request.path is '/api'
+	#assert request.method is 'POST'
+	return request
+
+
+if __name__ == '__main__':
+	app.run()
