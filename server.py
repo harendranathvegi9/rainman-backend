e2caa4880d97869f5bfd312cbba835857136240e
Seth Thompson <s3th.thompson@gmail.com>
2013-11-08 20:20:51 -0500

    basic stubs

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 import os
-from flask import Flask, request
+from flask import Flask, request,jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'Hello World!'
-@app.route('/api', methods=['POST'])
-def parse():
-	#assert request.path is '/api'
-	#assert request.method is 'POST'
-	return request
+def rainman(content, domain):
+	cards = []
+	return jsonify(content=content, cards=cards)
 
+@app.route('/')
+def home():
+	return 'Hello World!'
+
+@app.route('/api', methods=['POST'])
+def api():
+	content = request.args['content']
+	domain = request.args['domain']
+	return rainman(content, domain)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
