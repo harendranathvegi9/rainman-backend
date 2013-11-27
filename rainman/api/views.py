4f2cef7dfb670403f3aeb6bd3911e92c95875cb6
Seth Thompson <s3th.thompson@gmail.com>
2013-11-27 02:44:21 -0500

    fix url

--- rainman/api/views.py
+++ rainman/api/views.py
@@ -5,8 +5,8 @@ from ..parse import Parser
 api = Blueprint('api', __name__, url_prefix='/api')
 
 @crossdomain(origin='*', headers=["Accept", "Content-Type"])
-@api.route('/v1', methods=['POST', 'OPTIONS'])
-def v1():
+@api.route('/article', methods=['POST', 'OPTIONS'])
+def article():
   r = request.get_json()
   content = r['content']
   domain = r['url']
