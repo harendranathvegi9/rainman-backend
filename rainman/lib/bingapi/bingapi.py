4c51c370a3af2b9b6986818734ca34b9e05f2100
Seth Thompson <s3th.thompson@gmail.com>
2013-12-29 20:25:40 -0800

    tabs to spaces

--- rainman/lib/bingapi/bingapi.py
+++ rainman/lib/bingapi/bingapi.py
@@ -6,78 +6,78 @@ import urllib2
 import json
 
 class BingAPI:
-	#The base URL for all endpoints
-	BASE_URL = 'https://api.datamarket.azure.com/Bing/Search/v1'
+    #The base URL for all endpoints
+    BASE_URL = 'https://api.datamarket.azure.com/Bing/Search/v1'
 
-	def __init__(self):
-		"""
-		Initializes API wrapper
-		"""
+    def __init__(self):
+        """
+        Initializes API wrapper
+        """
 
-		import os
-		try:
-		    key = os.environ.get('BING_API')
-		    if not key:
-		        print('API key not found in environment variable BING_API.')
-		        sys.exit(0)
-		    if key == '':
-		        #The key file should't be blank
-		        print('API key appears to be blank, please run: export BING_API=YOUR_KEY_HERE')
-		        sys.exit(0)
-		    else:
-		        #setup the key
-		        self.apikey = key
-		except Exception as e:
-		    print(e)
+        import os
+        try:
+            key = os.environ.get('BING_API')
+            if not key:
+                print('API key not found in environment variable BING_API.')
+                sys.exit(0)
+            if key == '':
+                #The key file should't be blank
+                print('API key appears to be blank, please run: export BING_API=YOUR_KEY_HERE')
+                sys.exit(0)
+            else:
+                #setup the key
+                self.apikey = key
+        except Exception as e:
+            print(e)
 
-	def search(self, source, query, market):
-		"""
-		Generic wrapper for the Bing Search API.
+    def search(self, source, query, market):
+        """
+        Generic wrapper for the Bing Search API.
 
-		source: API Source, e.g. "News" or "Image"
-		query: The search term
-		market: Geographic market to search, e.g. "en-US"
+        source: API Source, e.g. "News" or "Image"
+        query: The search term
+        market: Geographic market to search, e.g. "en-US"
 
-		Returns a dict containing the API response.
-		"""
+        Returns a dict containing the API response.
+        """
 
-		query = urllib.quote(query)
+        query = urllib.quote(query)
 
-		# create credential for authentication
-		credentials = (':%s' % self.apikey).encode('base64')[:-1]
-		auth = 'Basic %s' % credentials
+        # create credential for authentication
+        credentials = (':%s' % self.apikey).encode('base64')[:-1]
+        auth = 'Basic %s' % credentials
 
-		url = BingAPI.BASE_URL + '/{0}?Query=%27{1}%27&Market=%27{2}%27&$format=json'.format(source, query, market)
+        url = BingAPI.BASE_URL + '/{0}?Query=%27{1}%27&Market=%27{2}%27&$format=json'.format(source, query, market)
 
-		request = urllib2.Request(url)
-		request.add_header('Authorization', auth)
-		request_opener = urllib2.build_opener()
-		response = request_opener.open(request)
-		response_data = response.read()
+        request = urllib2.Request(url)
+        request.add_header('Authorization', auth)
+        request_opener = urllib2.build_opener()
+        response = request_opener.open(request)
+        response_data = response.read()
 
-		raw_result = json.loads(response_data)
+        raw_result = json.loads(response_data)
 
-		# Process weird API format to get an array of result objects
-		result = raw_result['d']['results']
+        # Process weird API format to get an array of result objects
+        result = raw_result['d']['results']
 
-		return result
+        return result
 
-	def news(self, query):
-		"""
-		Convenience method to search Bing News API.
-		Hardcoded to search en-US market for now.
+    def news(self, query):
+        """
+        Convenience method to search Bing News API.
+        Hardcoded to search en-US market for now.
 
-		query: The search term.
-		"""
-		return self.search("News", query, "en-US")
+        query: The search term.
+        """
+        return self.search("News", query, "en-US")
 
-	def image(self, query):
-		"""
-		Convenience method to search Bing Image API.
-		Hardcoded to search en-US market for now.
+    def image(self, query):
+        """
+        Convenience method to search Bing Image API.
+        Hardcoded to search en-US market for now.
 
-		query: The search term.
-		"""
-		return self.search("Image", query, "en-US")
+        query: The search term.
+        """
+        return self.search("Image", query, "en-US")
 
 
